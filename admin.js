8b0f8246d6f6d04c69772bfe2d6bb76d194873e8
JamesSchiller <schillerj78@gmail.com>
2020-07-14 05:56:24 -0400

    sort by user id and add new columns

--- admin.js
+++ admin.js
@@ -26,37 +26,85 @@ async function main() {
     })
     const users = await res.json();
     var html = `
-        <div class="row">
-            <div class="col-1">index</div>
-            <div class="col-2">userId</div>
-            <div class="col-3">email</div>
-            <div class="col-2">created</div>
-            <div class="col-1">aliasCount</div>
-            <div class="col-1">mailCount</div>
-            <div class="col-2">
-                operations
-            </div>
-        </div>`
+            <thead>
+                <tr>
+                    <th onclick="sortTable(0)">User ID</th>
+                    <th>Signup Timestamp</th>
+                    <th onclick="sortTable(2)">Signup email address</th>
+                    <th>Operation</th>
+                    <th>Total aliases</th>
+                    <th>Active aliases</th>
+                    <th>Total emails sent to user</th>
+                    <th>Total email replies by user</th>
+                    <th>Timestamp most recent email opened</th>
+                    <th>Timestamp of last alias created</th>
+                    <th>Total recipient email addresses</th>
+                </tr>
+            </thead>
+            <tbody>` 
+        
     users.forEach((user, i) => {
         html += ` 
-            <div class="row">
-                <div class="col-1">${i+1}</div>
-                <div class="col-2">${user.userId}</div>
-                <div class="col-3">${user.email}</div>
-                <div class="col-2">${user.created}</div>
-                <div class="col-1">${user.aliasCount}</div>
-                <div class="col-1">${user.mailcount}</div>
-                <div class="col-2">
-                    <button id="view-${i}">view</button>
-                </div>
-            </div>`
+                <tr>
+                    <td>${user.userId}</td>
+                    <td>${user.created}</td>
+                    <td>${user.email}</td>
+                    <td><button id="view-${i}">view</button></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>`
     });
-    document.getElementById('usertable').innerHTML = html;
+    html += `</tbody>`
+
+    document.getElementById('userTable').innerHTML = html;
     users.forEach((user, i) => {
         document.getElementById(`view-${i}`).addEventListener('click', async e => {
             window.location = `/user.html?id=${user.userId}`;
         });
     });
+    sortTable(0);
+}
+
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("userTable");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        switchcount ++;
+      } else {
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
 }
 
 main();
