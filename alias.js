620c1b2ab270f57df54dc4c54d3b9d3cb925a288
JamesSchiller <schillerj78@gmail.com>
2020-07-14 20:15:58 -0400

    alias details

--- alias.js
+++ alias.js
@@ -12,6 +12,45 @@ async function main() {
     document.getElementById('aliasAddress').innerHTML = alias.data[0].alias;
     document.getElementById('creationTimestamp').innerHTML = alias.data[0].created;
     document.getElementById('creationUrl').innerHTML = alias.data[0].refferedUrl;
+    var historyofActiveOrInactiveTogglesAndTimestampsTableHtml = `
+        <thead>
+            <th>Total emails sent through alias during active/inactive period</th>
+            <th>Total emails held/discarded when inactive</th>
+            <th>Total email replies to sender through alias during active/inactive period</th>
+        </thead>
+        </tbody>`;
+    historyofActiveOrInactiveTogglesAndTimestampsTableHtml += '</tbody>';
+    document.getElementById('historyofActiveOrInactiveTogglesAndTimestampsTable').innerHTML = historyofActiveOrInactiveTogglesAndTimestampsTableHtml;
+    var listOfEmailMessagesProcessedToAliasTableHtml = `
+    <thead>
+        <th>Email Message ID</th>
+        <th>Sender email address</th>
+        <th>Subject</th>
+        <th>Processed timestamp</th>
+        <th>Send/Held/Discarded status</th>
+        <th>Sent timestamp</th>
+        <th>alias ID</th>
+        <th>alias email address</th>
+        <th>Recipient email address(es)</th>
+        <th>Opened/Unopened/Bounced</th>
+        <th>Opened timestamp</th>
+    </thead>
+    </tbody>`;
+    listOfEmailMessagesProcessedToAliasTableHtml += '</tbody>';
+    document.getElementById('listOfEmailMessagesProcessedToAliasTable').innerHTML = listOfEmailMessagesProcessedToAliasTableHtml;
+    var listOfEmailRepliesToSenderFromAliasTableHtml = `
+    <thead>
+        <th>Email Message ID</th>
+        <th>To email address(es)</th>
+        <th>Subject</th>
+        <th>Processed timestamp</th>
+        <th>Alias active/inactive at time of send</th>
+        <th>Opened/Unopened/Bounced</th>
+        <th>Opened timestamp</th>
+    </thead>
+    </tbody>`;
+    listOfEmailRepliesToSenderFromAliasTableHtml += '</tbody>';
+    document.getElementById('listOfEmailRepliesToSenderFromAliasTable').innerHTML = listOfEmailRepliesToSenderFromAliasTableHtml;
     document.getElementById('rawAlias').innerHTML = JSON.stringify(alias, null, 2);
 }
 
