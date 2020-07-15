767a92eb483a34a8a0d88818125c19b54ffcb9f8
JamesSchiller <schillerj78@gmail.com>
2020-07-14 23:27:52 -0400

    config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // const API = 'http://localhost:3000';
 
 // test
-const API = 'https://jinnmailadmintest.herokuapp.com/';
+const API = 'https://jinnmailadmintest.herokuapp.com';
 
 // prod
 // const API = 'https://jinnmailapp.herokuapp.com';
